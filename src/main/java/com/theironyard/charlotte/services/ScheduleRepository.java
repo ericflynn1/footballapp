5ccefea6a6d9c28000fadeb532a3310ec7d40429
ericflynn1 <ericgflynn@gmail.com>
2019-04-04 15:17:42 -0700

    Update ScheduleRepository.java

--- src/main/java/com/theironyard/charlotte/services/ScheduleRepository.java
+++ src/main/java/com/theironyard/charlotte/services/ScheduleRepository.java
@@ -6,9 +6,6 @@ import org.springframework.stereotype.Component;
 
 import java.util.List;
 
-/**
- * Created by mfahrner on 10/12/16.
- */
 public interface ScheduleRepository extends CrudRepository<Schedule, Integer> {
     List<Schedule> findByHomeOrAway(String teamAbreviation, String something);
     List<Schedule> findByDayOfYear(int dayOfYear);
