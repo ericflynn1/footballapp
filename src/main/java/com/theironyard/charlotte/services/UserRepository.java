f181a3dcf7148296c984590c8ebd093b2c91138b
ericflynn1 <ericgflynn@gmail.com>
2019-04-04 15:18:33 -0700

    Update UserRepository.java

--- src/main/java/com/theironyard/charlotte/services/UserRepository.java
+++ src/main/java/com/theironyard/charlotte/services/UserRepository.java
@@ -3,9 +3,6 @@ package com.theironyard.charlotte.services;
 import com.theironyard.charlotte.entities.User;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by mfahrner on 10/4/16.
- */
 public interface UserRepository extends CrudRepository<User, Integer>{
     User findFirstByUserName(String userName);
 }
